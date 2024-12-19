4723950f0609077558c2d1329a373280d3c466b0
JamesGersava <jamesdaniel.gersava@hcdc.edu.ph>
2024-12-19 21:37:56 +0800

    Initial commit with backend, frontend, and env

---
+++ frontend/src/components/ToDoForm.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ToDoForm = () => {
+
+
+
+
+    return (  
+
+        <div>
+            <input type="text"  placeholder="Add Todo" className="input input-bordered input-info w-full max-w-xs"/>
+            <button className="btn btn-primary ml-2">Add todo</button>
+        </div>
+    ) 
+  }
+
+export default ToDoForm
